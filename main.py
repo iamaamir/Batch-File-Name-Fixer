2b5c7df2f274f055cfdef89d3759be6176c56399
Aamir khan <aamirkhan180@gmail.com>
2017-01-17 10:35:50 +0530

    total rename fix

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author: Varun Upadhyay
+
 from os import listdir, chdir, rename
 
-# Valid chars in a file name
 valid_chars = 'abcdefghijklmnopqrstuvwxyz0123456789._'
-dir_path = input("Enter the file path")
-dir_list = listdir(dir_path)
-total_files = len(dir_list)
+path = input("Enter Folder path: ")
+dirlist = listdir(path)
+total_files = len(dirlist)
+renamed = 0
 
-for i in dir_list:
-    ans = ''
-    for j in i:
-        if j.lower() in valid_chars:
-            ans += j
+chdir(path)
+for filename in dirlist:
+    name = ''
+    for char in filename:
+        if char.lower() in valid_chars:
+            name += char
         else:
-            if j == ' ':
-                ans += '_'
-    chdir(dir_path)
-    rename(i, ans)
+            if char == ' ':
+                name += '_'
 
-# Getting the final count of files in the folder after renaming
-# this is bad idea must count only renamed files 
-final_count = len(dir_list)
+    if name != filename:
+        rename(filename, name)
+        renamed += 1
 
 # Final Verification
 print('Number of files present initially: ', total_files)
-print('Number of file renamed: ', final_count)
-print('Difference: ', (total_files - final_count))
+print('Number of file renamed: ', renamed)
+print('Difference: ', (total_files - renamed))
