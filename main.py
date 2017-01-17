dfef76ec66684ac7bc923ddd8caf7bfc5c52bf72
Aamir khan <aamirkhan180@gmail.com>
2017-01-17 10:36:23 +0530

    an example with regex added

--- main.py
+++ main.py
@@ -27,3 +27,27 @@ for filename in dirlist:
 print('Number of files present initially: ', total_files)
 print('Number of file renamed: ', renamed)
 print('Difference: ', (total_files - renamed))
+
+
+# this is the way of doing the same thing with regex
+# but beware of "\" while using this
+
+# import re
+# invalid_chars = re.compile("[^a-zA-z\d\s\._]")
+# path = input("Enter the Folder path: ")
+# dir_list = listdir(path)
+# total_files = len(dir_list)
+# total_renamed = 0
+
+# chdir(path)
+# for name in dir_list:
+#     new_name = re.sub(invalid_chars, "", name).replace(' ', '_')
+#     if name != new_name:
+#         rename(name, new_name)
+#         total_renamed += 1
+
+
+# # Final Verification
+# print('Number of files present initially: ', total_files)
+# print('Number of file Renamed: ', total_renamed)
+# print('Difference: ', (total_files - total_renamed))
